69be208991787ca837e7b2ae863e3261445ea306
Pedro <pedrod33@ua.pt>
2021-11-23 15:54:52 +0000

    can draw, change colors, width and clear

--- color_segmenter.py
+++ color_segmenter.py
@@ -4,12 +4,24 @@ import cv2
 import json
 import sys
 from colorama import *
-# define a video capture object
-
-#callback para as trackbars
 
 
-def limit_image(B, G, R, final_threshred=None):
+def find_centroid(th):
+    ret=False
+    contours, hierarchy = cv2.findContours(th, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    cX=0
+    cY=0
+    for c in contours:
+        M = cv2.moments(c)
+        if M["m00"]==0:
+            cX=0
+            cY=0
+        else:
+            cX = int(M["m10"] / M["m00"])
+            cY = int(M["m01"] / M["m00"])
+            ret=True
+    return ret, (cX, cY)
+def limit_image(B, G, R):
     _, thresh1 = cv2.threshold(B, cv2.getTrackbarPos('Blue_max', 'frame'), 255, cv2.THRESH_BINARY_INV)
     _, thresh2 = cv2.threshold(B, cv2.getTrackbarPos('Blue_min', 'frame'), 255, cv2.THRESH_BINARY)
     _, thresh3 = cv2.threshold(G, cv2.getTrackbarPos('Red_max', 'frame'), 255, cv2.THRESH_BINARY_INV)
@@ -33,9 +45,12 @@ def segment(data, windowname, frame):
 
 
     # Capture the video frame by frame
-    (B, G, R) = cv2.split(frame)
+    B=frame[:,:,0]
+    G=frame[:,:,1]
+    R=frame[:,:,2]
     img_w_thresh = limit_image(B, G, R)
+    ret, pos = find_centroid(img_w_thresh)
     # Display the resulting frame
-    return img_w_thresh
+    return ret, pos
 
 
